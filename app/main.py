f7bc17a08ae28c26b9a4493a8d2f5bdf455c230b
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-01-21 15:37:35 +0200

    Implement CleanUpFile to remove file after context

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 class CleanUpFile:
-    # write your code here
-    pass
+    import os
+
+
+class CleanUpFile:
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
